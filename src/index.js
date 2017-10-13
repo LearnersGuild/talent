ff19eabb104a5eff2d9be3eedbb7e69cc383888d
Doug L <douglubaway@gmail.com>
2017-10-13 14:18:59 -0700

    Create navbar component, add react-bootstrap, change routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import {Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 import App from './components/app';
 import Home from './components/home';
+import TalentNavbar from './components/talentNavbar';
 
 ReactDOM.render(
   <BrowserRouter>
-    <div>
-        <Route exact path="/" component={App} />
-        <Route path="/home" component={Home} />
-    </div>
+    <Route exact path="/" component={TalentNavbar} />
   </BrowserRouter>
   , document.querySelector('.container'));
