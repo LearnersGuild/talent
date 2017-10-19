e0dd2c864fcbe3546ae9db9501099821916d4694
Doug L <douglaslubaway@gmail.com>
2017-10-19 15:11:12 -0700

    Completes profile page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import {Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 import css from '../public/index.scss'
-import {tempInfo, userTempInfo, fakeDB, fakeProjects} from './db/mock-data'
+import {tempInfo, userTempInfo, fakeDB, fakeProjects, experience, skills} from './db/mock-data'
 
 import TalentNavbar from './components/talentNavbar';
 import CollectionPage from './pages/collection/index';
@@ -19,7 +19,7 @@ ReactDOM.render(
         <Route exact path="/" render={() =>
           <CollectionPage data={fakeDB} info={tempInfo[0]} projects={fakeProjects}/>} />
         <Route path="/learners/:githubHandle" render={() =>
-          <ProfilePage info={userTempInfo} projects={fakeProjects}/>} />
+          <ProfilePage info={userTempInfo[0]} experience={experience} skills={skills} projects={fakeProjects}/>} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
---
+++ src/pages/profile/profile/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
+
+import Blurb from '../../../components/blurb';
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.info.name}</h2>
+        <div className="row">
+          <img className="img-responsive col-sm-5" src="/LearnerImage.png" />
+          <Blurb className="col-sm-4" info={this.props.info} />
+        </div>
+        <ButtonToolbar className="row">
+          <DropdownButton bsSize="large" title="Personal Contact Information" id="dropdown-size-large">
+            <MenuItem eventKey="1">Action</MenuItem>
+            <MenuItem eventKey="2">Another action</MenuItem>
+            <MenuItem eventKey="3">Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey="4">Separated link</MenuItem>
+          </DropdownButton>
+        </ButtonToolbar>
+      </div>
+    )
+  }
+}
