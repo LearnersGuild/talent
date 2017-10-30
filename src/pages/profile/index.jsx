a3964758411fdc864d1cd8b566732c2ea6b97d81
Doug L <douglaslubaway@gmail.com>
2017-10-30 12:31:06 -0700

    Completes profile page (#24)

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 
-import Blurb from '../../components/blurb'
-import Projects from '../../components/projects'
+import Blurb from '../../components/blurb';
+import Profile from './profile';
+import Projects from '../../components/projects';
 
 export default class ProfilePage extends Component {
   render() {
-    return this.props.info.map(learner => {
-      return (
-        <div>
-          <h2>{learner.name}</h2>
-          <h3>{learner.about}</h3>
-          <Projects projects={this.props.projects}/>
+    return (
+    <div className="container">
+      <Profile info={this.props.info} />
+      <div className="row">
+        <div className="col-lg-6">
+          <Blurb info={this.props.experience}/>
         </div>
-      )
-    })
+        <div className="col-lg-6">
+          <Blurb info={this.props.skills}/>
+        </div>
+      </div>
+      <h2 className="text-center">Projects</h2>
+      <Projects projects={this.props.projects}/>
+    </div>
+  );
   }
 }
