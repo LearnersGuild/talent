7be3bb579ad4d63d7023b8133d96df04017b6580
Serafin <leaf466@gmail.com>
2017-10-30 15:55:19 -0700

    Express server now serves react

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, '../../public')))
 
-app.get('/', (req, res) => {
-  console.log('hello world');
+app.get('/api/ohNo', (req, res) => {
+  res.send('Im an PIE')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'))
 })
 
 app.listen(port, () => {
