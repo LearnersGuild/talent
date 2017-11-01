6f3aea85b2401331041363f1163e75f6b275ccf3
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-10-31 17:47:39 -0700

    gets projects to render in homepage

--- src/client/components/projects/index.jsx
+++ src/client/components/projects/index.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Projects extends Component {
+ 
   render() {
-    return this.props.projects.map(project => {
-      return (
-        <div key={project.id}>
-          <h4><a href={project.link}>{project.title}</a></h4>
-        </div>
-      );
+    console.log('in projects',this.props.projects)
+    return this.props.projects.map((array) => {
+      return array.map(project => {
+         return (
+          <div key={project.id}>
+            <h4><a href={project.link}>{project.title}</a></h4>
+          </div>
+        );
+      })
     });
   }
 }
