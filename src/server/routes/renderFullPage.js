334a6ce4faef4848283d0341a53d8a653e8a6cab
leikkisa <sallymaki@gmail.com>
2017-11-01 14:57:37 -0700

    Clean up renderHTML to remove duplicates

--- src/server/routes/renderFullPage.js
+++ src/server/routes/renderFullPage.js
@@ -7,11 +7,10 @@ export default function renderFullPage(html) {
       <link rel="stylesheet" href="/style.css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
       <title>Talent</title>
-    <link href="/style.css" rel="stylesheet"></head>
+    </head>
     <body>
       <div class="container">${html}</div>
     <script type="text/javascript" src="/bundle.js"></script></body>
-    <script src="/bundle.js"></script>
   </html>
   `
 }
