26eea8dc3d3ddbbaaff7de4938700efdf0aee77c
leikkisa <sallymaki@gmail.com>
2017-11-01 16:59:46 -0700

    remove dist from gitignore and delete dist

--- dist/client/routes/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var routes = ['/', '/learners', '/learners/:githubHandle'];
-
-exports.default = routes;
--- dist/server/routes/renderFullPage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = renderFullPage;
-function renderFullPage(html) {
-  return "\n  <!DOCTYPE html>\n  <html>\n    <head>\n      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n      <link rel=\"stylesheet\" href=\"/style.css\">\n      <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css\">\n      <title>Talent</title>\n    </head>\n    <body>\n      <div class=\"container\">" + html + "</div>\n    <script type=\"text/javascript\" src=\"/bundle.js\"></script></body>\n  </html>\n  ";
-}
