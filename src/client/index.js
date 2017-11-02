7225b09906844602d75620a3726945cef402089b
leikkisa <sallymaki@gmail.com>
2017-11-02 14:23:45 -0700

    Clean up code

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom'
-import { BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
-import {Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
-
-import css from '../../public/index.scss'
-import {tempInfo, userTempInfo, fakeDB, fakeProjects, experience, skills} from '../server/db/mock-data'
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app'
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
