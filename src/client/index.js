b2747b0295a4e2042b0f9957d8c24d0ffba3e96c
leikkisa <sallymaki@gmail.com>
2017-11-13 16:07:45 -0800

    Remove one routes file and clean up dependencies

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 import ReduxPromise from 'redux-promise'
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 import App from './components/app'
 import reducers from './reducers'
