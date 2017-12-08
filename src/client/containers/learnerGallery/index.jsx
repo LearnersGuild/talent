aef9217b912374ba376bf35a0477b419ba3005bc
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-12-08 14:52:34 -0800

    Refactored learner gallery container to fire a dispatch to deal with whether the container is loading or not, rather than in local state. Added another action named doneLoading. Renamed learners key in the redux store to guild key. Various other minor changes

--- src/client/containers/learnerGallery/index.jsx
+++ src/client/containers/learnerGallery/index.jsx
@@ -1,50 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CollectionPage from '../collection';
+import { bindActionCreators } from 'redux';
 import _ from 'lodash';
+import { doneLoading } from '../../actions';
 
 class LearnerGallery extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      currentView: this.filterLearner(this.props.type),
-      loading: false,
-    };
+    if (this.props.type) {
+      this.state = {
+        selectedLearners: this.filterByType(this.props.type),
+      };
+    } else {
+      this.state = {
+        selectedLearners: this.filterBySkill(),
+      };
+    }
+
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentWillMount() {
-    this.setState({
-      loading: true,
-    });
-  }
-
   componentDidMount() {
-    this.setState({
-      loading: false,
-    });
+    this.props.doneLoading();
   }
 
   handleChange (event) {
     event.preventDefault();
-    this.setState({ currentView: event.target.value });
+    this.setState({ selectedLearners: event.target.value });
   }
 
   filterByName () {
-    const filteredLearner = this.filterLearner(this.props.type);
-    if (Array.isArray(this.state.currentView)) {
+    let filteredLearner;
+    if (this.props.type) {
+      filteredLearner = this.filterByType(this.props.type);
+    } else {
+      filteredLearner = this.filterBySkill();
+    }
+    if (Array.isArray(this.state.selectedLearners)) {
       return filteredLearner;
     }
     return filteredLearner.filter(learner => {
-      if (learner.name.toLowerCase().startsWith(this.state.currentView.toLowerCase())) {
+      if (learner.name.toLowerCase().startsWith(this.state.selectedLearners.toLowerCase())) {
         return learner;
       }
     });
   }
 
-  filterLearner (type) {
-    return this.props.learners.filter(learner => {
+  filterBySkill () {
+    let searchSkill = this.props.match.params.searchSkill.replace(/search=/, '').split(',');
+    return this.props.guild.learners.filter(learner => {
+      const objectKeys = Object.values(learner.skills).map(object => object.skills);
+      for (let i = 0; i < searchSkill.length; i++) {
+        if (objectKeys.includes(searchSkill[i])) {
+          if (i + 1 === searchSkill.length) {
+            return learner;
+          }
+        } else {
+          break;
+        }
+      }
+    });
+  }
+
+  filterByType (type) {
+    return this.props.guild.learners.filter(learner => {
       if (type === 'alumni') {
         if (learner.alumni === true) {
           return learner;
@@ -55,18 +76,6 @@ class LearnerGallery extends Component {
         }
       } else if (type === 'all') {
         return learner;
-      } else if (type === undefined) {
-        let searchSkill = this.props.match.params.searchSkill.replace(/search=/, '').split(',');
-        const objectKeys = Object.values(learner.skills).map(object => object.skills);
-        for (let i = 0; i < searchSkill.length; i++) {
-          if (objectKeys.includes(searchSkill[i])) {
-            if (i + 1 === searchSkill.length) {
-              return learner;
-            }
-          } else {
-            break;
-          }
-        }
       }
     });
   }
@@ -83,7 +92,7 @@ class LearnerGallery extends Component {
           <input type="text" placeholder="search" onChange={this.handleChange}></input>
         </form>
         {
-          this.state.loading ? (<div className="flex-center"><img className="lg-loading" src="/LearnerLogo.png" /></div>) : (<CollectionPage
+          this.props.guild.loading ? (<div className="flex-center"><img className="lg-loading" src="/LearnerLogo.png" /></div>) : (<CollectionPage
                     data={this.filterByName()}
                     info={ { name: 'About Learners Guild', story: 'This is just a sentence.' } }
                     projects={this.getProjects(this.filterByName())}
@@ -94,8 +103,12 @@ class LearnerGallery extends Component {
   }
 }
 
-function mapStateToProps({ learners }) {
-  return { learners };
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ doneLoading }, dispatch);
 }
 
-export default connect(mapStateToProps)(LearnerGallery);
+function mapStateToProps({ guild }) {
+  return { guild };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LearnerGallery);
