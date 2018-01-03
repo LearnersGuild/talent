55540e88b042d827de3de6b9bc8902bd082bc4c6
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-01-03 14:27:53 -0800

    Started adding testing to architecture.md

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
+import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app';
 import reducers from './reducers';
