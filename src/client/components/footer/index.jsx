4fad6946fcdce4a3c592505f231afba6e1158dd4
Jonathan Lubaway <NeoJax@gmail.com>
2018-01-11 16:56:53 -0800

    Styling with pokemon

--- src/client/components/footer/index.jsx
+++ src/client/components/footer/index.jsx
@@ -4,20 +4,7 @@ import { Navbar } from 'react-bootstrap';
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <div className='filler'>
-          <br></br>
-        </div>
-        <span></span>
-        <div className='footer'>
-          <div className='firstAngle'></div>
-          <span></span>
-          <div className='secondAngle'></div>
-          <span></span>
-          <div className='red-box'>
-          </div>
-        </div>
-      </div>
+      <div className='footer'></div>
     );
   }
 }
