e1ea8caa22439e2d6ff31ab3b3ab5bfbfd4589dc
Jonathan Lubaway <NeoJax@gmail.com>
2018-01-22 16:07:01 -0800

    Made the Navbar into purely CSS

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -23,6 +23,7 @@ router.use('*', (req, res) => {
           Please enable JavaScript to view this page.</b>
         </div>
       </noscript>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
       <link rel="stylesheet" href="/style.css">
