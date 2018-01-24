c6d13a4ded167ff8e64cf138f45a013d5bb76942
Jonathan Lubaway <NeoJax@gmail.com>
2018-01-24 13:51:17 -0800

    Made changes to MergeHelper to help with making IDs for everything and added my personal JSON

--- src/server/data/mergeHelper.js
+++ src/server/data/mergeHelper.js
@@ -7,11 +7,23 @@ const buildLearners = () => {
       throw error;
     }
 
-    // let count = 0;
+    let count = 0;
     files.forEach((file) => {
       let data = require(`./learners/${file}`);
-      // data.learners.id = count;
-      // count++;
+      data.id = count;
+      count++;
+      data.projects.forEach((project) => {
+        project.id = count;
+        count++;
+      });
+      data.skills.forEach((skill) => {
+        skill.id = count;
+        count++;
+      });
+      data.experience.forEach((exp) => {
+        exp.id = count;
+        count++;
+      });
       allLearners.push(data);
     });
     return allLearners;
