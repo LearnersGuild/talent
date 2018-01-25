f88cd82399b22c1b3a5679cc6ce8d02735aae935
Jonathan Lubaway <NeoJax@gmail.com>
2018-01-25 13:12:29 -0800

    Changed the example JSONs and the schema by removing the id's which are now automatically put in place

--- src/server/data/validate.js
+++ src/server/data/validate.js
@@ -1,12 +1,25 @@
+const fs = require('fs');
 const Ajv = require('Ajv');
 const schema = require('./schema.json');
-const data = require('./learners/SonyaSims.json');
 const ajv = new Ajv();
 const validate = ajv.compile(schema);
-const valid = validate(data);
 
-if (!valid) {
-  console.log(validate.errors);
-} else {
-  console.log('Format is correct');
-}
+const checkLearners = () => {
+  fs.readdir('./learners', 'utf8', (error, files) => {
+    if (error) {
+      throw error;
+    }
+
+    files.forEach((file) => {
+      let data = require(`./learners/${file}`);
+      const valid = validate(data);
+      if (!valid) {
+        console.log(validate.errors);
+      } else {
+        console.log('Format is correct');
+      }
+    });
+  });
+};
+
+checkLearners();
