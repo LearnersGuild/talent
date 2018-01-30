3f418d198d89c04c9746be2a7d82992f0ba2b331
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-01-30 14:36:37 -0800

    Started webpack configuration to compile css, need to fix bug where node cannot find css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -22,11 +22,8 @@ module.exports = {
         loader: 'url',
       }, {
         test: /\.(css)?$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader'],
-          publicPath: '/public'
-        })
+        include: __dirname + '/src',
+        loaders: ExtractTextPlugin.extract('css-loader'),
       }, {
         test: /\.(js|jsx)?$/,
         loader: 'babel-loader',
