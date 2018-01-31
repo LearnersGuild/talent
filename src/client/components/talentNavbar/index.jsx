98420d9936889a1930f49aeda9cdb9a5c84b71d1
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-01-31 13:56:02 -0800

    Broke up the style sheet in app index.css into multiple stylesheets, added the webpack compiled css sheet to the gitignore

--- src/client/components/talentNavbar/index.jsx
+++ src/client/components/talentNavbar/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
+import './index.css';
 
 export default class TalentNavbar extends Component {
 
--- src/client/containers/collection/index.jsx
+++ src/client/containers/collection/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './index.css';
 import UserGallery from './userGallery';
 import Projects from '../../components/projects';
 import Blurb from '../../components/blurb';
@@ -8,7 +8,7 @@ export default class CollectionPage extends Component {
   render() {
     return (
       <div>
-      <div className="flex-column-home-page">
+      <div className="flex-column-search-page">
         <Blurb className="col-lg-1 pagination-center" info={this.props.info} />
         <UserGallery data={this.props.data}/>
       </div>
