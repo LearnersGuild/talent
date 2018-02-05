b3931a7131608afc8072be4a8f04eb542b74c034
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-05 15:59:11 -0800

    take out axios call and separate concern to redux-saga & lean out action creator calls, taking out unused creators

--- src/client/containers/loading/index.jsx
+++ src/client/containers/loading/index.jsx
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchLearners, setSkills, searchByName, doneLoading } from '../../actions';
-import axios from 'axios';
+
+import { fetchLearnersRequest } from '../../actions';
 import './index.css';
 
 class Loading extends Component {
 
   componentDidMount() {
-    axios.get('http://localhost:3000/api/learners')
-    .then(response => response.data)
-    .then(data => this.props.fetchLearners(data))
-    .then(() => this.props.searchByName())
-    .then(() => this.establishNames())
-    .then(skills => this.props.setSkills(skills))
-    .then(() => this.props.doneLoading())
-    .catch(error => {
-      this.props.doneLoading();
-      console.log('Error fetching and parsing data: ', error);
-      throw error;
-    });
+    this.props.fetchLearnersRequest('http://localhost:3000/api/learners');
   }
 
   establishNames() {
@@ -58,7 +46,9 @@ class Loading extends Component {
     return (
       <div>
         {
-          this.props.guild.loading ? (<div><div className="flex-center"><img className="lg-loading" src="/LearnerLogo.png" /></div><div className="footer-filler"></div></div>) : this.props.children
+          this.props.guild.loading
+            ? (<div><div className="flex-center"><img className="lg-loading" src="/LearnerLogo.png" /></div><div className="footer-filler"></div></div>)
+            : this.props.children
         }
       </div>
     );
@@ -69,8 +59,4 @@ function mapStateToProps({ guild }) {
   return { guild };
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchLearners, setSkills, doneLoading, searchByName }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Loading);
+export default connect(mapStateToProps, { fetchLearnersRequest })(Loading);
