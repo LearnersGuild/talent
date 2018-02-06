5083d5619b8e98657c57bd49ca1a1d68fc56fd30
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-05 16:03:41 -0800

    add request success and failure states to reducer

--- src/client/reducers/reducer-learner.jsx
+++ src/client/reducers/reducer-learner.jsx
@@ -1,23 +1,33 @@
 import {
-  FETCH_LEARNERS,
   DONE_LOADING,
   SET_SKILLS,
   SEARCH_BY_SKILL,
   SEARCH_BY_NAME
 } from '../actions/types';
 
+import {
+  FETCH_LEARNERS_SUCCESS,
+  FETCH_LEARNERS_FAILURE
+} from '../actions/types'
+
 export default function(state = { loading: true }, action) {
   switch (action.type) {
-    case FETCH_LEARNERS:
+    case FETCH_LEARNERS_SUCCESS:
       return {
         learners: action.payload,
         loading: action.loading,
       };
+<<<<<<< HEAD
     case SET_SKILLS:
       return {
         learners: state.learners,
         loading: state.loading,
         skills: action.skills,
+=======
+    case FETCH_LEARNERS_FAILURE:
+      return {
+        error: action.error
+>>>>>>> add request success and failure states to reducer
       };
     case DONE_LOADING:
       return {
