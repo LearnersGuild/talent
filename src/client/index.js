91e7698af9da28fd1b99584c6451f649cd49feae
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-05 16:01:59 -0800

    wire up redux-saga and redux-logger middleware to store

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './components/app';
 import reducers from './reducers';
+import mySaga from './sagas/'
+
+const sagaMiddleware = createSagaMiddleware();
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(
+    loggerMiddleware,
+    sagaMiddleware
+  )
+)
+
+sagaMiddleware.run(mySaga);
 
 ReactDOM.hydrate(
-  <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={ store }>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
