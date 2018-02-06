584fdb5294df5f1c19ba189de5a0463c554c95fa
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-02-06 15:44:20 -0800

    Removed brackets, typo

--- src/client/components/talentNavbar/index.jsx
+++ src/client/components/talentNavbar/index.jsx
@@ -11,7 +11,7 @@ export default class TalentNavbar extends Component {
           <input type="checkbox" name="hamburger" id="hamburger"></input>
           <label htmlFor="hamburger" className="navbar-icon">&#9776;</label>
           <h2 className="navbar-title">TALENT</h2>
-          <div className="talent-list">{
+          <div className="talent-list">
               <ul className="talent-nav">
                 <li className="talent-item">
                   <Link to="/">Home</Link>
@@ -29,7 +29,6 @@ export default class TalentNavbar extends Component {
                   <Link to="/skills">Search By Skills</Link>
                 </li>
               </ul>
-            }
           </div>
         </div>
       </div>
