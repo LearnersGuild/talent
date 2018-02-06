738b2db10d5059b62e4232fbdcb910001ab1581e
Jonathan Lubaway <NeoJax@gmail.com>
2018-02-06 14:28:29 -0800

    Fixed the navbar for 375 px mobile viewing and normal laptop viewing

--- src/client/components/talentNavbar/index.jsx
+++ src/client/components/talentNavbar/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
 import './index.css';
 
 export default class TalentNavbar extends Component {
@@ -8,35 +7,31 @@ export default class TalentNavbar extends Component {
   render() {
     return (
       <div className="talent-container">
-        <Navbar className="talent-navbar" fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <input type="checkbox" name="hamburger" id="hamburger"></input>
-              <label htmlFor="hamburger" className="navbar-icon glyphicon glyphicon-menu-hamburger"></label>
-              <h2 className="navbar-title">TALENT</h2>
-              <div className="talent-list">{
-                  <ul className="talent-nav">
-                    <li className="talent-item">
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li className="talent-item">
-                      <Link to="/current">Current</Link>
-                    </li>
-                    <li className="talent-item">
-                      <Link to="/alumni">Alumni</Link>
-                    </li>
-                    <li className="talent-item">
-                      <Link to="/learners">All Learners</Link>
-                    </li>
-                    <li className="talent-item">
-                      <Link to="/skills">Search By Skills</Link>
-                    </li>
-                  </ul>
-                }
-              </div>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
+        <div className="talent-navbar">
+          <input type="checkbox" name="hamburger" id="hamburger"></input>
+          <label htmlFor="hamburger" className="navbar-icon">&#9776;</label>
+          <h2 className="navbar-title">TALENT</h2>
+          <div className="talent-list">{
+              <ul className="talent-nav">
+                <li className="talent-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="talent-item">
+                  <Link to="/current">Current</Link>
+                </li>
+                <li className="talent-item">
+                  <Link to="/alumni">Alumni</Link>
+                </li>
+                <li className="talent-item">
+                  <Link to="/learners">All Learners</Link>
+                </li>
+                <li className="talent-item">
+                  <Link to="/skills">Search By Skills</Link>
+                </li>
+              </ul>
+            }
+          </div>
+        </div>
       </div>
     );
   }
