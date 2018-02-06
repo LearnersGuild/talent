171286a7638b9c18e45cdf1a0d4dfd4a3b01d12f
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-02-06 14:04:34 -0800

    Fixed bug where some images were sourced relatively and not appearing on page

--- src/client/components/projects/index.jsx
+++ src/client/components/projects/index.jsx
@@ -7,7 +7,7 @@ export default class Projects extends Component {
     return this.props.projects.map(project => {
         return (
           <span key={project.id}>
-            <a href={project.link} target="__blank" ><img className="project-image" src={project.title}></img></a>
+            <a href={project.link} target="__blank" ><img className="project-image" src={`/${project.title}`}></img></a>
           </span>
         );
       });
