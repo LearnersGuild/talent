f53c3ef1a5d12925722f41a94c9985d18d955de3
Judy Tuan <judytuna@gmail.com>
2018-02-08 17:09:19 -0800

    [pull/74] merge fix: add set skills from neojax's PR 72

--- src/client/actions/types.js
+++ src/client/actions/types.js
@@ -1,4 +1,5 @@
 export const FETCH_LEARNERS = 'fetch_learners';
+export const SET_SKILLS = 'set_skills';
 export const DONE_LOADING = 'done_loading';
 export const SEARCH_BY_SKILL = 'search_by_skill';
 export const SEARCH_BY_NAME = 'search_by_name';
