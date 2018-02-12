a780310042853be680b948fc871dfd7e57e9cb0c
Jonathan Lubaway <NeoJax@gmail.com>
2018-02-12 15:07:10 -0800

    Rebasing off master

--- src/server/data/makeLearner.js
+++ src/server/data/makeLearner.js
@@ -1,14 +1,31 @@
 const fs = require('fs');
 const learner = {};
 
-const makeLearner = (github, linkedin, twitter, name, story, projects, skills, experience, alumni) => {
-  learner.github_handle = github;
-  learner.linkedin_profile = linkedin;
-  learner.twitter = twitter;
-  learner.name = name;
-  learner.story = story;
-  learner.projects = projects;
-  learner.skills = skills;
-  learner.experience = experience;
-  learner.alumni = alumni;
-};
+fs.readFile(`../../../public/Example Learner's Form.csv`, 'utf-8', (error, data) => {
+  let learnerData = data.split('\r');
+  learnerData = learnerData.splice(1);
+  learnerData.pop();
+  for (let i = 0; i < learnerData.length; i++) {
+    let learnerProfile = [];
+    let learnerProject = [];
+    let learnerSkills = [];
+    let learnerExperience = [];
+    learnerData[i] = learnerData[i].split(',');
+    learnerData[i] = learnerData[i].splice(1);
+    for (let j = 0; j < 5; j++) {
+      learnerProfile.push(learnerData[i].shift());
+    }
+
+    while (true) {
+      console.log(learnerData[i]);
+      if (learnerData[i][0].includes('\"')) {
+        learnerProject.push(learnerData[i].shift());
+        break;
+      }
+
+      learnerProject.push(learnerData[i].shift());
+    }
+
+    console.log(learnerData);
+  }
+});
