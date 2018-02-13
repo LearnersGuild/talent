297d4e49f29a8d4a85e428058397ca427f20df49
pkallas <pkallas29@gmail.com>
2018-02-13 14:28:31 -0800

    v2 of project styling, less javascript is running, but less pics on the page

--- src/client/containers/collection/index.jsx
+++ src/client/containers/collection/index.jsx
@@ -4,27 +4,14 @@ import UserGallery from './userGallery';
 import Projects from '../../components/projects';
 
 export default class CollectionPage extends Component {
-  makeProjectRows() {
-    let projectRows = [];
-    for (let i = 0; i < this.props.projects.length; i += 4) {
-      projectRows.push(this.props.projects.slice(i, i + 4));
-    }
-
-    return projectRows;
-  }
-
-  renderProjects() {
-    const projectRows = this.makeProjectRows();
-    return projectRows.map((row, index) => {
-      return (
-        <div className="gallery-project-images" key={index} >
-          <Projects projects={row} />
-        </div>
-      );
-    });
+  generateRandomNumber() {
+    const maxNumber = this.props.projects.length - 4;
+    return Math.floor(Math.random() * maxNumber);
   }
 
   render() {
+    const slicePosition1 = this.generateRandomNumber();
+    const slicePosition2 = slicePosition1 + 4;
     return (
       <div>
       <div className="flex-column-search-page">
@@ -32,7 +19,9 @@ export default class CollectionPage extends Component {
       </div>
         <h2 className="text-center">Projects</h2>
         <div className="gallery-page-container">
-          { this.renderProjects() }
+          <div className="gallery-project-images">
+            <Projects projects={this.props.projects.slice(slicePosition1, slicePosition2)} />
+          </div>
         </div>
         <div className="footer-filler"></div>
       </div>
