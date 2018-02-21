52ce9b06a5b0879ff4aae05a2e2f2abd5e5f5817
Jay Cribas <feverjay@gmail.com>
2018-02-20 16:15:10 -0800

    Adds social media links to each learner on gallery page

---
+++ src/client/components/socialLinks/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+const Link = (props) => {
+  const imageSrc = `/Logos/${props.type}.svg`
+  return (
+    <a href={props.url}><img className="social-logo" src={imageSrc}/></a>
+  )
+}
+
+export default function(props) {
+  const githubLink = `https://github.com/${props.github}`
+  const linkedinLink = `https://www.linkedin.com/in/${props.linkedin}`
+  const twitterLink = `https://twitter.com/${props.twitter}`
+
+  return (
+    <div className='text-center'>
+      <Link url={linkedinLink} type='linkedin'/>
+      <Link url={githubLink} type='github'/>
+      <Link url={twitterLink} type='twitter'/>
+    </div>
+  );
+}
--- src/client/containers/collection/userBadge/index.jsx
+++ src/client/containers/collection/userBadge/index.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SocialLinks from '../../../components/socialLinks';
 import './index.css';
 
 export default class UserBadge extends Component {
   render() {
-    return this.props.data.map(learner => {
+    return this.props.data.map((learner) => {
       return (
         <span key={learner.id} className="learner-span">
-          <Link to={`/learners/${learner.github_handle}`}><img className="learner-image" src="/LearnerImage.png" /> </Link>
+          <Link to={`/learners/${learner.github_handle}`}><img className="learner-image" src="/LearnerImage.png" /></Link>
+          <SocialLinks
+            github={learner.github_handle}
+            linkedin={learner.linkedin_profile}
+            twitter={learner.twitter}
+          />
           <p className="text-center">{learner.name}</p>
         </span>
       );
