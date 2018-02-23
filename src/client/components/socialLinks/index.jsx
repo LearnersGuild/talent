a166c469fe6e1a1ee7be2b85d83a13f2fa793fd0
Jay Cribas <feverjay@gmail.com>
2018-02-23 14:05:57 -0800

    Fixes eslint errors

--- src/client/components/socialLinks/index.jsx
+++ src/client/components/socialLinks/index.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import './index.css';
 
 const Link = (props) => {
-  const imageSrc = `/Logos/${props.type}.svg`
+  const imageSrc = `/Logos/${props.type}.svg`;
   return (
-    <a href={props.url}><img className="social-logo" src={imageSrc}/></a>
-  )
-}
+    <a target="_blank" href={props.href}>
+      <img className="social-logo" src={imageSrc} alt={`${props.type}-logo`} />
+    </a>
+  );
+};
 
-export default function(props) {
-  const githubLink = `https://github.com/${props.github}`
-  const linkedinLink = `https://www.linkedin.com/in/${props.linkedin}`
-  const twitterLink = `https://twitter.com/${props.twitter}`
+export default function (props) {
+  const githubLink = `https://github.com/${props.github}`;
+  const linkedinLink = `https://www.linkedin.com/in/${props.linkedin}`;
+  const twitterLink = `https://twitter.com/${props.twitter}`;
 
   return (
-    <div className='social-logos text-center'>
-      <Link url={linkedinLink} type='linkedin'/>
-      <Link url={githubLink} type='github'/>
-      <Link url={twitterLink} type='twitter'/>
+    <div className="social-logos text-center">
+      <Link href={linkedinLink} type="linkedin" />
+      <Link href={githubLink} type="github" />
+      <Link href={twitterLink} type="twitter" />
     </div>
   );
 }
