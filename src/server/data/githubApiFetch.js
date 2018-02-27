708879fe315704b0c549bdd1e37c1efdca763474
Jay Cribas <feverjay@gmail.com>
2018-02-27 14:49:07 -0800

    Moves write function into fetch call

--- src/server/data/githubApiFetch.js
+++ src/server/data/githubApiFetch.js
@@ -1,30 +1,36 @@
 const fs = require('fs');
 const axios = require('axios');
 
-function githubApiFetch(learners) {
-  learners.forEach((file) => {
-    let learner = require(`./learners/${file}`);
-    const url = `https://api.github.com/users/${learner.github_handle}`;
-    const githubResponse = getGithubAvatar(url);
-    const avatarUrl = githubResponse.avatar_url;
-    learner.avatar_url = avatarUrl;
-    const updatedLearner = JSON.stringify(learner);
-    try {
-      fs.writeFile(`./src/server/data/learners/${learner}`, updatedLearner)
-    } catch(error) {
-      console.log("🔥🔥ERROR!!! (╯°□°）╯︵ ┻━┻🔥🔥", error)
+function learners() {
+  fs.readdir('./src/server/data/learners', 'utf8', (error, files) => {
+    if (error) {
+      console.log("🔥🔥error (╯°□°）╯︵ ┻━┻🔥🔥", error)
     }
+    return githubApiFetch(files)
   })
 }
 
-function getGithubAvatar(url) {
-  return axios.get(url)
-    .then((response) => {
-      console.log("🔥🔥response.data (╯°□°）╯︵ ┻━┻🔥🔥", response.data)
-      return response.data;
-    })
-    .catch((error) => {
-      console.log('Error fetching and parsing data: ', error);
-      throw error;
-    });
+function githubApiFetch(learners) {
+  learners.forEach((file, index) => {
+    let learner = require(`./learners/${file}`);
+    const url = `https://api.github.com/users/${learner.github_handle}`;
+    return axios.get(url)
+      .then((response) => {
+        const avatarUrl = response.data.avatar_url;
+        console.log("🔥🔥avatarUrl (╯°□°）╯︵ ┻━┻🔥🔥", avatarUrl)
+        learner.avatar_url = avatarUrl;
+        const updatedLearner = JSON.stringify(learner, null, 2);
+        try {
+          fs.writeFile(`./src/server/data/learners/${learners[index]}`, updatedLearner);
+        } catch(error) {
+          console.log("🔥🔥ERROR!!! (╯°□°）╯︵ ┻━┻🔥🔥", error)
+        }
+      })
+      .catch((error) => {
+        console.log('Error fetching and parsing data: ', error);
+        throw error;
+      });
+  })
 }
+
+learners();
