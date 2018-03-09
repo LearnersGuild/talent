887689e547c1a19d6ef14e245dc1a3d6218b7b52
Johnathon Lubaway <NeoJax@gmail.com>
2018-03-09 15:54:32 -0800

    Made a bunch of changes namely stuff dealing with makeLearner.js

--- src/client/components/projects/index.jsx
+++ src/client/components/projects/index.jsx
@@ -5,6 +5,7 @@ export default class Projects extends Component {
 
   render() {
     return this.props.projects.map(project => {
+      console.log(project);
         return (
           <span key={project.id}>
             <a href={project.link} target="__blank" ><img className="project-image" src={`/${project.title}`}></img></a>
